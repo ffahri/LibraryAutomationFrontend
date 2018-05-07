f899a35acf00655e2511ddcc3f7371094dcc5cbb
ffahri <ffahri@users.noreply.github.com>
2018-05-07 10:12:35 +0300

    Initial Commit

---
+++ src/main/java/com/webischia/libraryfrontend/LibraryfrontendApplication.java
@@ -0,0 +1,12 @@
+package com.webischia.libraryfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryfrontendApplication.class, args);
+	}
+}

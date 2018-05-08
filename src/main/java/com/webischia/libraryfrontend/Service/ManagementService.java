d1ee8622c05679938f38d3b965cdb01d26b25ca4
ffahri <ffahri@users.noreply.github.com>
2018-05-08 23:40:28 +0300

    new methods types added (yay more copy-paste)

--- src/main/java/com/webischia/libraryfrontend/Service/ManagementService.java
+++ src/main/java/com/webischia/libraryfrontend/Service/ManagementService.java
@@ -1,6 +1,6 @@
 package com.webischia.libraryfrontend.Service;
 
-import com.webischia.libraryfrontend.Model.Author;
+import com.webischia.libraryfrontend.Model.*;
 
 import java.util.List;
 
@@ -10,4 +10,26 @@ public interface ManagementService {
     void deleteAuthor(int id,String token);
     Author showAuthor(int id,String token);
     Author[] getAllAuthors(String token);
+
+    void addSubject(Subject subject,String token);
+    void deleteSubject(int id,String token);
+    Subject showSubject(int id,String token);
+    Subject[] getAllSubjects(String token);
+
+    void addItemType(ItemType itemType,String token);
+    void deleteItemType(int id,String token);
+    ItemType showItemType(int id,String token);
+    ItemType[] getAllTypes(String token);
+
+    void addItem(Items items,String token);
+    void deleteItem(int id,String token);
+    Items showItem(int id,String token);
+    Items[] getAllItems(String token);
+
+    void addPublisher(Publisher publisher,String token);
+    void deletePublisher(int id,String token);
+    Publisher showPublisher(int id,String token);
+    Publisher[] getAllPublisher(String token);
+
+
 }
